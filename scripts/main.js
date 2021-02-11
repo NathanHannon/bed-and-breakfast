9729a84c16f3a51d3938232a0079439149a90f87
Nathan Hannon <nathan@hannon.me>
2021-02-10 21:29:30 -0500

    added custom date picker and modal functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+let attention = Prompt();
+
+// Validation and error checking
 (function () {
     'use strict';
     window.addEventListener('load', function () {
@@ -15,3 +18,48 @@
         });
     }, false);
 })();
+
+// Notification banners with the notie library
+function notify(message, messageType) {
+    notie.alert({
+        type: messageType,
+        text: message
+    })
+}
+
+// Custom date picker with the vanillajs-datepicker library 
+const elem = document.getElementById('reservationDates');
+const rangepicker = new DateRangePicker(elem, {
+    format: "yyyy-mm-dd"
+});
+
+// Modal function with the sweetalert2 library
+function notifyModal(title, text, icon, confirmationButtonText) {
+    Swal.fire({
+        title: title,
+        html: text,
+        icon: icon,
+        confirmButtonText: confirmationButtonText
+    })
+}
+
+// JavaScript module for prompting users
+function Prompt() {
+    let toast = function () {
+        console.log("clicked button and got toast")
+    }
+    let success = function () {
+        console.log("clicked button and got success")
+    }
+    return {
+        toast: toast,
+        success: success,
+    }
+}
+
+// Test Button
+document.getElementById("btnTest").addEventListener("click", function () {
+    // notify("This is my Message", "warning");
+    // notifyModal("title", "<em>Hello World!</em>", "success", "Text for my Button");
+    attention.success();
+})
