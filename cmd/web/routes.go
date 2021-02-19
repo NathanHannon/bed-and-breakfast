ef5b2de75bd5166af816fa2923f6e1980091aaf9
Nathan Hannon <nathan@hannon.me>
2021-02-19 10:36:49 -0500

    asdded route for static assets

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,15 +11,20 @@ import (
 
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
+	fileServer := http.FileServer(http.Dir("./static/"))
+
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	mux.Use(middleware.Recoverer)
 	mux.Use(NoSurf)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
-	mux.Get("/make-reservation", handlers.Repo.Reservation)
-	mux.Get("/search-availability", handlers.Repo.Availability)
 	mux.Get("/generals-quarters", handlers.Repo.Generals)
 	mux.Get("/majors-suite", handlers.Repo.Majors)
+
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+
 	return mux
 }
