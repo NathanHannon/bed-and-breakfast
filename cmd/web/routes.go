439a0e8d86789baf687ab4fe494088ccb5605624
Nathan Hannon <nathan@hannon.me>
2021-02-23 22:28:07 -0500

    Added comments for handler blocks

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,19 +13,23 @@ func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 	fileServer := http.FileServer(http.Dir("./static/"))
 
+	// Middleware handlers
 	mux.Use(middleware.Recoverer)
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
 
+	// Static file handler
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
+	// Misc. page handlers
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 	mux.Get("/generals-quarters", handlers.Repo.Generals)
 	mux.Get("/majors-suite", handlers.Repo.Majors)
 	mux.Get("/contact", handlers.Repo.Contact)
-
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
+
+	// Search page handlers
 	mux.Get("/search-availability", handlers.Repo.Availability)
 	mux.Post("/search-availability", handlers.Repo.PostAvailability)
 
