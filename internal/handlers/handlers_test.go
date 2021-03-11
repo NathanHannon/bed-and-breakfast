9badd44dc91484b00cf745628f4907956bfaf418
Nathan Hannon <nathan@hannon.me>
2021-03-10 22:58:57 -0500

    started adding handler tests

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -1,42 +1,19 @@
-package main
+package handlers
 
 import (
 	"encoding/gob"
-	"fmt"
 	"log"
-	"net/http"
-	"time"
+	"net/http"	"time"
 
 	"github.com/alexedwards/scs/v2"
-	"github.com/nathanhannon/bed-and-breakfast/internal/config"
-	"github.com/nathanhannon/bed-and-breakfast/internal/handlers"
 	"github.com/nathanhannon/bed-and-breakfast/internal/models"
 	"github.com/nathanhannon/bed-and-breakfast/internal/render"
 )
 
-const portNumber = ":8081"
-
 var app config.AppConfig
 var session *scs.SessionManager
 
-// main is the main function
-func main() {
-
-	err := run()
-
-	fmt.Println((fmt.Sprintf("Starting port number on port %s", portNumber)))
-
-	srv := &http.Server{
-		Addr:    portNumber,
-		Handler: routes(&app),
-	}
-	err = srv.ListenAndServe()
-	log.Fatal(err)
-}
-
-func run() error {
-
-	// What is going to be put into the session
+func getRoutes() http.Handler {
 	gob.Register(models.Reservation{})
 
 	// Change this to true when in production
@@ -53,7 +30,6 @@ func run() error {
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
-		return err
 	}
 	app.TemplateCache = tc
 	app.UseCache = false
@@ -61,6 +37,4 @@ func run() error {
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
 	render.NewTemplates(&app)
-
-	return nil
 }
