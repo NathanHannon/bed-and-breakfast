3023a06c3557fa6284a9b4a38bff5bcd13b065a5
Nathan Hannon <nathan@hannon.me>
2021-03-11 22:08:39 -0500

    started adding data to handlers tests

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -1,1 +1,16 @@
 package handlers
+
+type postData struct {
+	key   string
+	value string
+}
+
+var tests = []struct {
+	name               string
+	url                string
+	method             string
+	params             []postData
+	expectedStstusCode int
+}{
+	{},
+}
