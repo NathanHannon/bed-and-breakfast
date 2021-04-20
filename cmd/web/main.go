7d911e48b54bec1e6070c4386d6f6d8dd4cb9da2
Nathan Hannon <nathan@hannon.me>
2021-04-19 23:16:28 -0400

    started adding helper functions for logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
@@ -18,13 +19,15 @@ const portNumber = ":8081"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 // main is the main function
 func main() {
 
 	err := run()
 
-	fmt.Println((fmt.Sprintf("Starting port number on port %s", portNumber)))
+	fmt.Printf("Starting port number on port %s", portNumber)
 
 	srv := &http.Server{
 		Addr:    portNumber,
@@ -41,12 +44,18 @@ func run() error {
 	// Change this to true when in production
 	app.InProduction = false
 
+	// Logging and error handling
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
+	// Session info
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
 	session.Cookie.SameSite = http.SameSiteLaxMode
 	session.Cookie.Secure = app.InProduction
-
 	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
