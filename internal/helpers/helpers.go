1a91afa42df17e78a4d6e6f79b6150f223951442
Nathan Hannon <nathan@hannon.me>
2021-04-20 15:49:28 -0400

    added error functions

--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -1,9 +1,22 @@
 package helpers
 
-import "github.com/nathanhannon/bed-and-breakfast/internal/config"
+import (
+	"net/http"
+
+	"github.com/nathanhannon/bed-and-breakfast/internal/config"
+)
 
 var app *config.AppConfig
 
+// NewHelpers sets up app config for helpers
 func NewHelpers(a *config.AppConfig) {
 	app = a
+}
+
+func ClientError(w http.ResponseWriter, status int) {
+
+}
+
+func ServerError(w http.ResponseWriter, err error) {
+
 }
