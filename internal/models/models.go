5ebfbf3bbd5929698ff4dd3a040fac2c321f27b1
Nathan Hannon <nathan@hannon.me>
2021-05-01 20:23:51 -0400

    Set up models

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,9 +1,66 @@
 package models
 
+import "time"
+
 // Reservation holds reservation data
 type Reservation struct {
 	FirstName string
 	LastName  string
 	Email     string
 	Phone     string
+}
+
+// Users is the user model
+type Users struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	AccessLevel string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+// Rooms is the room model
+type Rooms struct {
+	ID        int
+	RoomName  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// Resstrictions is the restriction model
+type Restrictions struct {
+	ID              int
+	RestrictionName string
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+// Reservations is the reservation model
+type Reservations struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Email     string
+	StartDate time.Time
+	EndDate   time.Time
+	RoomID    int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Room      Rooms
+}
+
+type RoomRestrictions struct {
+	ID            int
+	StartDate     time.Time
+	EndDate       time.Time
+	RoomID        int
+	ReservationID int
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Room          Rooms
+	Reservation   Reservations
+	Restriction   Restrictions
 }
