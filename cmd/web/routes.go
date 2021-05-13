3c3ec4474c37707841f23ea466336ebe75a04f28
Nathan Hannon <nathan@hannon.me>
2021-05-13 14:53:04 -0400

    started adding post login functionality

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -27,7 +27,6 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/generals-quarters", handlers.Repo.Generals)
 	mux.Get("/majors-suite", handlers.Repo.Majors)
 	mux.Get("/contact", handlers.Repo.Contact)
-	mux.Get("/login", handlers.Repo.Login)
 
 	// Search page handlers
 	mux.Get("/search-availability", handlers.Repo.Availability)
@@ -40,6 +39,10 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
 	mux.Post("/make-reservation", handlers.Repo.PostReservation)
 	mux.Get("/reservation-summary", handlers.Repo.ReservationSummary)
+
+	// Login page handlers
+	mux.Get("/login", handlers.Repo.Login)
+	mux.Post("/login", handlers.Repo.PostLogin)
 
 	return mux
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -319,13 +319,6 @@ func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
 	render.Template(w, r, "contact.page.tmpl", &models.TemplateData{})
 }
 
-// Login handles the login page
-func (m *Repository) Login(w http.ResponseWriter, r *http.Request) {
-	render.Template(w, r, "login.page.tmpl", &models.TemplateData{
-		Form: forms.New(nil),
-	})
-}
-
 // ChooseRoom extracts the room ID from session data and pre-fills it in the reservation form
 func (m *Repository) ChooseRoom(w http.ResponseWriter, r *http.Request) {
 	roomID, err := strconv.Atoi(chi.URLParam(r, "id"))
@@ -374,4 +367,15 @@ func (m *Repository) BookRoom(w http.ResponseWriter, r *http.Request) {
 
 	m.App.Session.Put(r.Context(), "reservation", res)
 	http.Redirect(w, r, "/make-reservation", http.StatusSeeOther)
+}
+
+// Login handles the login page
+func (m *Repository) Login(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, r, "login.page.tmpl", &models.TemplateData{
+		Form: forms.New(nil),
+	})
+}
+
+func (m *Repository) PostLogin(w http.ResponseWriter, r *http.Request) {
+
 }
