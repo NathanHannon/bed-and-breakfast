3c61264656ee58f5e902fcda8ea7388c23e57353
Nathan Hannon <nathan@hannon.me>
2021-08-25 15:43:08 -0400

    fixed flag in script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-go build -o bed-and-breakfast cmd/web/*.go && ./bed-and-breakfast -dname=bed-and-breakfast -dbuser=nathan -cache=false -production=false
+go build -o bed-and-breakfast cmd/web/*.go && ./bed-and-breakfast -dbname=bed-and-breakfast -dbuser=nathan -cache=false -production=false
